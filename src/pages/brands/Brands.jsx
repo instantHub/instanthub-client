0023245d717d4081efc199413f387229c98b2fbe
Yusuf Qureshi <yousuf337692qureshi@gmail.com>
2024-05-07 03:04:00 +0530

    Client Version 2

--- src/pages/brands/Brands.jsx
+++ src/pages/brands/Brands.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import brands from "../../data/brands.json";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useGetCategoryQuery, useGetBrandQuery } from "../../features/api";
