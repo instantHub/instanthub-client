c662630087d5d03539d3d2461d88b619b81d1d63
Yusuf Qureshi <yousuf337692qureshi@gmail.com>
2025-04-29 19:51:29 +0530

    fixed conditionlabels delete handler

--- src/admin/pages/questions/ConditionLabelsList.jsx
+++ src/admin/pages/questions/ConditionLabelsList.jsx
@@ -43,8 +43,8 @@ const ConditionLabelsTable = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [labelToDelete, setLabelToDelete] = useState("");
 
-  const handleDelete = async (category, conditionLabelId, conditionLabel) => {
-    console.log(category, conditionLabelId);
+  const handleDelete = async (labelData) => {
+    const [category, conditionLabelId, conditionLabel] = labelData;
     try {
       await deleteConditionLabel({ category, conditionLabelId });
       toast.success(`Successfully deleted Condition Label ${conditionLabel}`);
@@ -248,46 +248,3 @@ const ConditionLabelsTable = () => {
 };
 
 export default ConditionLabelsTable;
-
-// const ConfirmationModal = ({
-//   isOpen,
-//   onClose,
-//   onConfirm,
-//   itemToDelete,
-//   title = "Are you sure?",
-//   description = "This action cannot be undone.",
-//   confirmText = "Confirm",
-//   cancelText = "Cancel",
-//   detail = null,
-// }) => {
-//   if (!isOpen) return null;
-
-//   console.log("itemToDelete", itemToDelete);
-
-//   return (
-//     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-//       <div className="bg-white w-full max-w-md rounded shadow-lg p-6">
-//         <h2 className="text-lg font-semibold mb-4">{title}</h2>
-//         <p className="text-gray-800 mb-2">{detail && detail}</p>
-//         <p className="text-gray-600 mb-6">{description}</p>
-//         <div className="flex justify-end space-x-4">
-//           <button
-//             className="px-4 py-2 text-gray-600 bg-gray-100 hover:bg-gray-200 rounded"
-//             onClick={onClose}
-//           >
-//             {cancelText}
-//           </button>
-//           <button
-//             className="px-4 py-2 text-white bg-red-600 hover:bg-red-700 rounded"
-//             onClick={() => {
-//               onConfirm(...itemToDelete);
-//               onClose();
-//             }}
-//           >
-//             {confirmText}
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
